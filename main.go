3a564211cf357d388cca53a22bfeae06928b09b1
badc0re <dame.jovanoski@gmail.com>
2022-01-13 10:47:10 +0100

    First attempt parser

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/badc0re/hprog/lexer"
-	"github.com/badc0re/hprog/token"
+	"github.com/badc0re/hprog/parser"
 )
 
 func readline(idet string, scanner *bufio.Scanner) bool {
@@ -17,64 +13,67 @@ func readline(idet string, scanner *bufio.Scanner) bool {
 }
 
 func main() {
-	// direct file parser
-	//test()
-	var buffer []string
-	var inputFile string
-	flag.StringVar(&inputFile, "file", "", "Input hell file.")
-	flag.Parse()
+	/*
+		// direct file parser
+		//test()
+		var buffer []string
+		var inputFile string
+		flag.StringVar(&inputFile, "file", "", "Input hell file.")
+		flag.Parse()
 
-	fmt.Println(inputFile)
-	hFile, err := os.Open(inputFile)
+		fmt.Println(inputFile)
+		hFile, err := os.Open(inputFile)
 
-	if hFile != nil {
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fileScanner := bufio.NewScanner(hFile)
-		for fileScanner.Scan() {
-			buffer = append(buffer, fileScanner.Text())
-		}
+		if hFile != nil {
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fileScanner := bufio.NewScanner(hFile)
+			for fileScanner.Scan() {
+				buffer = append(buffer, fileScanner.Text())
+			}
 
-		lex := lexer.Init(strings.Join(buffer[:], "\n"))
-		for tkn := range lex.Consume() {
-			token.Print(tkn)
-		}
-	} else {
-		const idet = "hprog> "
+			lex := lexer.Init(strings.Join(buffer[:], "\n"))
+			for tkn := range lex.Consume() {
+				token.Print(tkn)
+			}
+		} else {
+			const idet = "hprog> "
 
-		fmt.Println("Hprog Version 0.01")
-		fmt.Println("One way to escape, ctr-c to exit.")
+			fmt.Println("Hprog Version 0.01")
+			fmt.Println("One way to escape, ctr-c to exit.")
 
-		scanner := bufio.NewScanner(os.Stdin)
+			scanner := bufio.NewScanner(os.Stdin)
 
-		onNewLine := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-			return bufio.ScanLines(data, atEOF)
-		}
+			onNewLine := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+				return bufio.ScanLines(data, atEOF)
+			}
 
-		scanner.Split(onNewLine)
-		for {
-			for readline(idet, scanner) {
-				var sline = scanner.Text()
-				if len(sline) > 0 {
-					var tokens []token.Token
-					lex := lexer.Init(sline)
-					for tkn := range lex.Consume() {
-						tokens = append(tokens, tkn)
-						if tkn.Type == token.EOF || tkn.Type == token.ERR {
-							break
+			scanner.Split(onNewLine)
+			for {
+				for readline(idet, scanner) {
+					var sline = scanner.Text()
+					if len(sline) > 0 {
+						var tokens []token.Token
+						lex := lexer.Init(sline)
+						for tkn := range lex.Consume() {
+							tokens = append(tokens, tkn)
+							if tkn.Type == token.EOF || tkn.Type == token.ERR {
+								break
+							}
+							token.Print(tkn)
 						}
-						token.Print(tkn)
-					}
 
-					buffer = append(buffer, sline)
+						buffer = append(buffer, sline)
+					}
 				}
 			}
-		}
 
-		if scanner.Err() != nil {
-			fmt.Printf("error: %s\n", scanner.Err())
+			if scanner.Err() != nil {
+				fmt.Printf("error: %s\n", scanner.Err())
+			}
 		}
-	}
+	*/
+	parser.TestParser("1+2")
 }
